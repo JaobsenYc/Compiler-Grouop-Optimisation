75596b346c273aae16e672e28ef9f013d67cc927
Chris <chrischeung.nier@gmail.com>
2021-04-10 13:43:22 +0800

    small changes

--- ConstantFolder.java
+++ ConstantFolder.java
@@ -1,15 +1,16 @@
 package comp0012.main;
-
-import org.apache.bcel.classfile.ClassParser;
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.bcel.classfile.Method;
-import org.apache.bcel.generic.*;
-import org.apache.bcel.util.InstructionFinder;
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.*;
+import org.apache.bcel.util.InstructionFinder;
 
 
 public class ConstantFolder
@@ -146,9 +147,7 @@ public class ConstantFolder
 		} catch (TargetLostException e) {
 			for (InstructionHandle target : e.getTargets()) {
 				for (InstructionTargeter targeter : target.getTargeters()) {
-					if (newInst != null) {
-						targeter.updateTarget(target, newInst);
-					}
+					targeter.updateTarget(target, newInst);
 				}
 			}
 		}
@@ -239,17 +238,19 @@ public class ConstantFolder
 			System.out.println(mgen.getInstructionList());
 		}
 	}
-	
+
 	public void write(String optimisedFilePath)
 	{
 		this.optimize();
 
 		try {
-			FileOutputStream out = new FileOutputStream(optimisedFilePath);
+			FileOutputStream out = new FileOutputStream(new File(optimisedFilePath));
 			this.optimized.dump(out);
 		} catch (FileNotFoundException e) {
+			// Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
