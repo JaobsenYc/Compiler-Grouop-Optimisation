eaa75cc5117fbad9ebecef0f1de0ec2166c626bd
preston-tong25 <72134429+preston-tong25@users.noreply.github.com>
2021-04-13 12:50:15 +0800

    Integrated simple folding with constant variable folding

--- ConstantFolder.java
+++ ConstantFolder.java
@@ -19,10 +19,6 @@ public class ConstantFolder
 	JavaClass original = null;
 	JavaClass optimized = null;
 
-	public static final String OP_ADD = "add";
-	public static final String OP_SUB = "sub";
-	public static final String OP_MUL = "mul";
-	public static final String OP_DIV = "div";
 
 	public ConstantFolder(String classFilePath)
 	{
@@ -36,9 +32,217 @@ public class ConstantFolder
 	}
 
 
+	private void simpleFolding(ClassGen cgen, ConstantPoolGen cpgen, Method method, InstructionList instList) {
+		System.out.println("Performing Simple Folding On > " + cgen.getClassName() + " > " + method.getName());
+		boolean optimised = true;
+		while (optimised){
+			optimised = false;
+			String regex = "(LDC|LDC2_W|ConstantPushInstruction) ConversionInstruction? (LDC|LDC2_W|ConstantPushInstruction) ConversionInstruction? ArithmeticInstruction";
+			InstructionFinder finder = new InstructionFinder(instList);
+			Iterator iterator = finder.search(regex);
+			while (iterator.hasNext()) {//...}
+				InstructionHandle[] instructions = (InstructionHandle[]) iterator.next();
+				int instNum = 0;
 
-	
-	
+				Number Num1 = getNum(instNum, cpgen, instructions);
+				instNum += 1;
+
+				ConversionInstruction conversion1 = getConversion(instNum, instructions);
+				if (conversion1 != null) {
+					instNum += 1;
+				}
+
+				Number Num2 = getNum(instNum, cpgen, instructions);
+				instNum += 1;
+
+				ConversionInstruction conversion2 = getConversion(instNum, instructions);
+				if (conversion2 != null) {
+					instNum += 1;
+				}
+
+				ArithmeticInstruction operation = getOperation(instNum, instructions);
+				Type numType = operation.getType(cpgen);
+				String opType = operation.getName().substring(1);
+
+				Number foldedValue = doOperation(Num1, Num2, numType, opType);
+				if (foldedValue != null) {
+					int constantPoolIndex = addToConstantPool(numType, foldedValue, cpgen);
+					if (constantPoolIndex != -1) {
+						System.out.println("Simple Folding Success!");
+						System.out.println("Before: ");
+						System.out.println(instList);
+						InstructionHandle newInst = addInst(numType, instList, instructions[0], constantPoolIndex);
+						deleteInst(instList, instructions[0], instructions[instNum], newInst);
+						optimised = true;
+						System.out.println("After: ");
+						System.out.println(instList);
+					}
+				}
+			}
+		}
+	}
+
+	private Number getNum(int instNum, ConstantPoolGen cpgen, InstructionHandle[] instructions) {
+		Number Num = null;
+		if (instructions[instNum].getInstruction() instanceof LDC) {
+			Num = (Number) ((LDC) instructions[instNum].getInstruction()).getValue(cpgen);
+		}
+		if (instructions[instNum].getInstruction() instanceof LDC2_W) {
+			Num = ((LDC2_W) instructions[instNum].getInstruction()).getValue(cpgen);
+		}
+		if (instructions[instNum].getInstruction() instanceof ConstantPushInstruction) {
+			Num = ((ConstantPushInstruction) instructions[instNum].getInstruction()).getValue();
+		}
+		return Num;
+	}
+
+	private ConversionInstruction getConversion(int instNum, InstructionHandle[] instructions) {
+		ConversionInstruction conversion = null;
+		if (instructions[instNum].getInstruction() instanceof ConversionInstruction) {
+			conversion = (ConversionInstruction) instructions[instNum].getInstruction();
+		}
+		return conversion;
+	}
+
+	private ArithmeticInstruction getOperation(int instNum, InstructionHandle[] instructions) {
+		ArithmeticInstruction operation = null;
+		if (instructions[instNum].getInstruction() instanceof ArithmeticInstruction) {
+			operation = (ArithmeticInstruction) instructions[instNum].getInstruction();
+		}
+		return operation;
+	}
+
+	private int addToConstantPool(Type numType, Number foldedValue, ConstantPoolGen cpgen) {
+		int constantPoolIndex = -1;
+		if (numType == Type.INT) {
+			constantPoolIndex = cpgen.addInteger(foldedValue.intValue());
+		}
+		if (numType == Type.LONG) {
+			constantPoolIndex = cpgen.addLong(foldedValue.longValue());
+		}
+		if (numType == Type.FLOAT) {
+			constantPoolIndex = cpgen.addFloat(foldedValue.floatValue());
+		}
+		if (numType == Type.DOUBLE) {
+			constantPoolIndex = cpgen.addDouble(foldedValue.doubleValue());
+		}
+		return constantPoolIndex;
+	}
+
+	private InstructionHandle addInst(Type numType, InstructionList instList, InstructionHandle position, int constantPoolIndex) {
+		InstructionHandle newInst = null;
+		boolean cond1 = (numType == Type.INT || numType == Type.FLOAT);
+		boolean cond2 = (numType == Type.LONG || numType == Type.DOUBLE);
+		if (cond1) {
+			newInst = instList.insert(position, new LDC(constantPoolIndex));
+		} else if (cond2) {
+			newInst = instList.insert(position, new LDC2_W(constantPoolIndex));
+		}
+		return newInst;
+	}
+
+	private void deleteInst(InstructionList instList, InstructionHandle first, InstructionHandle last, InstructionHandle newInst) {
+		try {
+			instList.delete(first, last);
+		} catch (TargetLostException e) {
+			for (InstructionHandle target : e.getTargets()) {
+				for (InstructionTargeter targeter : target.getTargeters()) {
+					targeter.updateTarget(target, newInst);
+				}
+			}
+		}
+	}
+
+	private Number doOperation(Number Num1, Number Num2, Type numType, String opType) {
+		Number result = null;
+		switch (opType) {
+			case "add":
+				result = add(Num1, Num2, numType);
+				break;
+			case "sub":
+				result = sub(Num1, Num2, numType);
+				break;
+			case "mul":
+				result = mul(Num1, Num2, numType);
+				break;
+			case "div":
+				result = div(Num1, Num2, numType);
+				break;
+		}
+		return result;
+	}
+
+	private Number add(Number Num1, Number Num2, Type numType) {
+		Number result = null;
+		if (numType == Type.INT) {
+			result = Num1.intValue() + Num2.intValue();
+		}
+		if (numType == Type.LONG) {
+			result = Num1.longValue() + Num2.longValue();
+		}
+		if (numType == Type.FLOAT) {
+			result = Num1.floatValue() + Num2.floatValue();
+		}
+		if (numType == Type.DOUBLE) {
+			result = Num1.doubleValue() + Num2.doubleValue();
+		}
+		return result;
+	}
+
+	private Number sub(Number Num1, Number Num2, Type numType) {
+		Number result = null;
+		if (numType == Type.INT) {
+			result = Num1.intValue() - Num2.intValue();
+		}
+		if (numType == Type.LONG) {
+			result = Num1.longValue() - Num2.longValue();
+		}
+		if (numType == Type.FLOAT) {
+			result = Num1.floatValue() - Num2.floatValue();
+		}
+		if (numType == Type.DOUBLE) {
+			result = Num1.doubleValue() - Num2.doubleValue();
+		}
+		return result;
+	}
+
+	private Number mul(Number Num1, Number Num2, Type numType) {
+		Number result = null;
+		if (numType == Type.INT) {
+			result = Num1.intValue() * Num2.intValue();
+		}
+		if (numType == Type.LONG) {
+			result = Num1.longValue() * Num2.longValue();
+		}
+		if (numType == Type.FLOAT) {
+			result = Num1.floatValue() * Num2.floatValue();
+		}
+		if (numType == Type.DOUBLE) {
+			result = Num1.doubleValue() * Num2.doubleValue();
+		}
+		return result;
+	}
+
+	private Number div(Number Num1, Number Num2, Type numType) {
+		Number result = null;
+		if (numType == Type.INT) {
+			result = Num1.intValue() / Num2.intValue();
+		}
+		if (numType == Type.LONG) {
+			result = Num1.longValue() / Num2.longValue();
+		}
+		if (numType == Type.FLOAT) {
+			result = Num1.floatValue() / Num2.floatValue();
+		}
+		if (numType == Type.DOUBLE) {
+			result = Num1.doubleValue() / Num2.doubleValue();
+		}
+		return result;
+	}
+
+
+
+
 	private void resolveComparison(ClassGen cgen, ConstantPoolGen cpgen, InstructionList il){
 		System.out.println("*** Optimization: resolve binary comaprison ***");
 
@@ -260,7 +464,7 @@ public class ConstantFolder
 		}
 	}
 
-	public void constantVariableFolding(ClassGen cgen, ConstantPoolGen cpgen, InstructionList il) {
+	public void constantVariableFolding(ClassGen cgen, ConstantPoolGen cpgen, Method method, InstructionList il) {
 		
 		// Higher view is to replace every load instruction with a push instruction from constant pool
 		// repeat this process until no more load instructions:
@@ -288,7 +492,7 @@ public class ConstantFolder
 		while(true) {
 
 			// 1. Run simple folding to get as many literals as possible. Addtional optimisation on goto statements.
-			doSimpleFolding(cgen, cpgen, il);
+			simpleFolding(cgen, cpgen, method, il);
 			System.out.println("***Done simple folding***");
 			resolveComparison(cgen, cpgen, il);
 
@@ -317,29 +521,42 @@ public class ConstantFolder
 		System.out.println("*** Completed constant variable folding ***");
 	}
 
+	private void optimizeMethod(ClassGen cgen, ConstantPoolGen cpgen, Method method) {
+		MethodGen mgen = new MethodGen(method, cgen.getClassName(), cpgen);
+		InstructionList instList = mgen.getInstructionList();
 
+		constantVariableFolding(cgen, cpgen, method, instList);
+		System.out.println(cgen.getClassName() + " > " + method.getName());
+		System.out.println(instList);
+		System.out.println("");
+		System.out.println("");
+
+
+		mgen.setMaxStack();
+		mgen.setMaxLocals();
+
+		Method newM = mgen.getMethod();
+		cgen.replaceMethod(method, newM);
+
+	}
 
 	
 	public void optimize()
 	{
-		ClassGen classGen = new ClassGen(original);
-		ConstantPoolGen constPoolGen = classGen.getConstantPool();
+		ClassGen cgen = new ClassGen(original);
+		ConstantPoolGen cpgen = cgen.getConstantPool();
 
+		// Implement your optimization here
 
-		// prints bytecode instruction list
-		Method[] methods = classGen.getMethods();
+		cgen.setMajor(50);
+
+		Method[] methods = cgen.getMethods();
 		for (Method method : methods) {
-			MethodGen methodGen = new MethodGen(method, classGen.getClassName(), constPoolGen);
-			InstructionList il = methodGen.getInstructionList();
-			constantVariableFolding(classGen, constPoolGen, il);
-			System.out.println(classGen.getClassName() + " > " + method.getName());
-			System.out.println(il);
-			System.out.println("");
-			System.out.println("");
-			
+			optimizeMethod(cgen, cpgen, method);
 		}
-        
-		this.optimized = gen.getJavaClass();
+
+
+		this.optimized = cgen.getJavaClass();
 	}
 
 	
